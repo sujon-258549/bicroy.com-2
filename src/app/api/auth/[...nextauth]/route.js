6d9f14f625e2608ac7cdc0367c11eeebb85a25e0
Md Sujon Mia <mia258549@gmail.com>
2024-10-05 05:04:04 +0600

    login

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,10 +1,24 @@
 
 import NextAuth from 'next-auth/next'
+import CredentialsProvider from 'next-auth/providers/credentials'
 
 const handler = async () => NextAuth({
 
-    session: {},
-    providers: [],
+    session: {
+        strategy: "jwt",
+        maxAge: " 30*24*60*60"
+    },
+    providers: [
+        CredentialsProvider({
+            credentials:{
+                email:{},
+                password:{}
+            },
+            async authorize (credentials){
+               return true;
+            }
+        })
+    ],
     collbacks: {},
     pages: {
         login: "/login"
